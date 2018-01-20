2df1671ddf6da4a1eb35c1d89e2f4fc2510e549c
Abhijeet Kumar <searchabhijeet@gmail.com>
2018-01-21 00:19:00 +0530

    model

--- first_app/views.py
+++ first_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from first_app.models import Topic, WebPage, AccessRecord
 
 
 def index(request):
-    my_dict = {'insert_me': "Hello, I'm from first app"}
+    date_access = AccessRecord.objects.order_by('date')
+    my_dict = {'insert_me': date_access}
     return render(request, 'first_app/index.html', context=my_dict)
